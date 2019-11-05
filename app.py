33fca5a942883bb0badaa1136692c104a22bddc4
Jimit Dholakia <jimitd101@gmail.com>
2019-11-05 15:25:37 +0530

    Train the model outside of Flask method
    Train the model beforehand so that prediction will be faster

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
 
+
 ## Definitions
 def remove_pattern(input_txt,pattern):
     r = re.findall(pattern,input_txt)
@@ -20,38 +21,40 @@ def count_punct(text):
 
 app = Flask(__name__)
 
+
+data = pd.read_csv("sentiment.tsv",sep = '\t')
+data.columns = ["label","body_text"]
+# Features and Labels
+data['label'] = data['label'].map({'pos': 0, 'neg': 1})
+data['tidy_tweet'] = np.vectorize(remove_pattern)(data['body_text'],"@[\w]*")
+tokenized_tweet = data['tidy_tweet'].apply(lambda x: x.split())
+stemmer = PorterStemmer()
+tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) 
+for i in range(len(tokenized_tweet)):
+    tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+data['tidy_tweet'] = tokenized_tweet
+data['body_len'] = data['body_text'].apply(lambda x:len(x) - x.count(" "))
+data['punct%'] = data['body_text'].apply(lambda x:count_punct(x))
+X = data['tidy_tweet']
+y = data['label']
+print(type(X))
+# Extract Feature With CountVectorizer
+cv = CountVectorizer()
+X = cv.fit_transform(X) # Fit the Data
+X = pd.concat([data['body_len'],data['punct%'],pd.DataFrame(X.toarray())],axis = 1)
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+## Using Classifier
+clf = LogisticRegression()
+clf.fit(X_train,y_train)
+
+
 @app.route('/')
 def home():
     return render_template('home.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    data = pd.read_csv("sentiment.tsv",sep = '\t')
-    data.columns = ["label","body_text"]
-    # Features and Labels
-    data['label'] = data['label'].map({'pos': 0, 'neg': 1})
-    data['tidy_tweet'] = np.vectorize(remove_pattern)(data['body_text'],"@[\w]*")
-    tokenized_tweet = data['tidy_tweet'].apply(lambda x: x.split())
-    stemmer = PorterStemmer()
-    tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) 
-    for i in range(len(tokenized_tweet)):
-        tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
-    data['tidy_tweet'] = tokenized_tweet
-    data['body_len'] = data['body_text'].apply(lambda x:len(x) - x.count(" "))
-    data['punct%'] = data['body_text'].apply(lambda x:count_punct(x))
-    X = data['tidy_tweet']
-    y = data['label']
-    print(type(X))
-    # Extract Feature With CountVectorizer
-    cv = CountVectorizer()
-    X = cv.fit_transform(X) # Fit the Data
-    X = pd.concat([data['body_len'],data['punct%'],pd.DataFrame(X.toarray())],axis = 1)
-    from sklearn.model_selection import train_test_split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    ## Using Classifier
-    clf = LogisticRegression()
-    clf.fit(X_train,y_train)
-
     if request.method == 'POST':
         message = request.form['message']
         data = [message]
@@ -61,7 +64,6 @@ def predict():
         total_data = pd.concat([body_len,punct,vect],axis = 1)
         my_prediction = clf.predict(total_data)
     return render_template('result.html',prediction = my_prediction)
-
 
 
 if __name__ == '__main__':
