aebe98d905bbae8efec184429b8fba56a6ec194d
mohammed rijwan <md1994rijwan@gmail.com>
2023-08-18 01:15:32 +0530

    Updated and resolved major issues.

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,29 @@
+# We are using an image which have all the ML packages
 FROM frolvlad/alpine-python-machinelearning:latest
 
 RUN pip install --upgrade pip
 
 WORKDIR /app
 
+# Copy the code to the working space
 COPY . /app
+
+# Adding necessary plugins
 RUN apk add build-base
 RUN apk add --no-cache --virtual .build-deps g++ python3-dev libffi-dev openssl-dev && \
-    apk add --no-cache --update python3 && \
     pip3 install --upgrade pip setuptools
+
+# Installing requirements.txt
 RUN pip3 install -r requirements.txt
-RUN python -m nltk.downloader punkt
+
+# Installing punkt used in our code
+RUN python -c "import nltk; nltk.download('punkt')"
+
+# Exposing the port
 EXPOSE 4000
 
+# Initializing python entrypoint and use app.py \
+# in cmd to run our application
 ENTRYPOINT  ["python"]
 
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import re
 import string
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import LogisticRegression
+import pickle
 
-
-## Definitions
+## Definitions to remove any patterns or punctuations from given statement
 def remove_pattern(input_txt,pattern):
     r = re.findall(pattern,input_txt)
     for i in r:
@@ -22,35 +22,10 @@ def count_punct(text):
 app = Flask(__name__)
 
 
-data = pd.read_csv("sentiment.tsv",sep = '\t')
-data.columns = ["label","body_text"]
-# Features and Labels
-data['label'] = data['label'].map({'pos': 0, 'neg': 1})
-data['tidy_tweet'] = np.vectorize(remove_pattern)(data['body_text'],"@[\w]*")
-tokenized_tweet = data['tidy_tweet'].apply(lambda x: x.split())
-stemmer = PorterStemmer()
-tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) 
-for i in range(len(tokenized_tweet)):
-    tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
-data['tidy_tweet'] = tokenized_tweet
-data['body_len'] = data['body_text'].apply(lambda x:len(x) - x.count(" "))
-data['punct%'] = data['body_text'].apply(lambda x:count_punct(x))
-X = data['tidy_tweet']
-y = data['label']
-# Extract Feature With CountVectorizer
-cv = CountVectorizer()
-X = cv.fit_transform(X) # Fit the Data
-X = pd.concat([data['body_len'],data['punct%'],pd.DataFrame(X.toarray())],axis = 1)
-from sklearn.model_selection import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-## Using Classifier
-clf = LogisticRegression(C=0.1, class_weight=None, dual=False, fit_intercept=True,
-                   intercept_scaling=1, l1_ratio=None, max_iter=100,
-                   multi_class='auto', n_jobs=None, penalty='l2',
-                   random_state=None, solver='liblinear', tol=0.0001, verbose=0,
-                   warm_start=False)
-clf.fit(X,y)
-
+model_filename = "finalized_model.sav"
+cv_filename = "finalized_count_vectoriser.sav"
+loaded_model = pickle.load(open(model_filename, 'rb'))
+loaded_cv = pickle.load(open(cv_filename, 'rb'))
 
 @app.route('/')
 def home():
@@ -59,13 +34,37 @@ def home():
 @app.route('/predict',methods=['POST'])
 def predict():
     if request.method == 'POST':
+        # We get the message from a form
         message = request.form['message']
         data = [message]
-        vect = pd.DataFrame(cv.transform(data).toarray())
-        body_len = pd.DataFrame([len(data) - data.count(" ")])
-        punct = pd.DataFrame([count_punct(data)])
-        total_data = pd.concat([body_len,punct,vect],axis = 1)
-        my_prediction = clf.predict(total_data)
+        
+        # we vectorise the input after treating with removing patterns
+        tidy_tweet = pd.Series(np.vectorize(remove_pattern)(data,"@[\w]*"))
+        
+        # split the string inputs
+        tokenized_tweet = tidy_tweet.apply(lambda x: x.split())
+        
+        # stem the inputs so that it create unbiased opinion for given model.
+        stemmer = PorterStemmer()
+        tokenized_tweet = tokenized_tweet.apply(lambda x: [stemmer.stem(i) for i in x]) 
+        for i in range(len(tokenized_tweet)):
+            tokenized_tweet[i] = ' '.join(tokenized_tweet[i])
+        
+        # Save the tokenized tweet, body length and punctations if they have
+        tokenized_tidy_tweet = tokenized_tweet
+        body_len = pd.Series(data).apply(lambda x:len(x) - x.count(" "))
+        punct = pd.Series(data).apply(lambda x:count_punct(x))
+        
+        # transform the tokentised tweet to get a vectorized format
+        X_result = loaded_cv.transform(tokenized_tidy_tweet)
+        
+        # concat all the 3 values to form a one input
+        input = pd.concat([body_len,punct,pd.DataFrame(X_result.toarray())],axis = 1).values
+        
+        # predicting the results
+        my_prediction = loaded_model.predict(input)
+        
+    # giving the results back to the html response
     return render_template('result.html',prediction = my_prediction)
 
 
